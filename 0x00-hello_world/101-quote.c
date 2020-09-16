f623e9029f12caf389872092ca4ead019bc0dd0a
Valerie Nierenberg <valerie.nierenberg@gmail.com>
2020-09-16 21:46:53 +0000

    Add 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+/**
+* main- void
+* description: Prints and that piece of art is useful" - Dora Korpar,
+* 2015-10-19
+* Return: 1
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	putchar("and that piece of art is useful"\" - Dora Korpar, 2015-10-19"
+
+	return (1);
+}
