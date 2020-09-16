ac7c518aeed69c7fde110d15433d6280748b2dbe
Valerie Nierenberg <valerie.nierenberg@gmail.com>
2020-09-16 21:48:17 +0000

    Add close paren and semicolon to 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@
 
 int main(void)
 {
-	putchar("and that piece of art is useful"\" - Dora Korpar, 2015-10-19"
+	putchar("and that piece of art is useful"\" - Dora Korpar, 2015-10-19");
 
 	return (1);
 }
