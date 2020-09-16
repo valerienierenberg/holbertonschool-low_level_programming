8149eb1b622db931e99987faae4cfee8886f16a5
Valerie Nierenberg <valerie.nierenberg@gmail.com>
2020-09-16 20:13:58 +0000

    Add 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+/**
+* main- void
+* description: puts "Programming is like building a multilingual puzzle"
+* Return: 0
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+
+	return (0);
+}
