8e08bdee07a7c3f160c092d1b93570e2f7794d86
Valerie Nierenberg <valerie.nierenberg@gmail.com>
2020-09-16 21:08:07 +0000

    Add bytes to printf output 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,11 +14,11 @@ int main(void)
 	long long int k;
 	float f;
 
-	printf("Size of a char: %i.\n", sizeof(c));
-	printf("Size of an int: %i.\n", sizeof(i));
-	printf("Size of a long int: %f.\n", sizeof(l));
-	printf("Size of a long long int: %f.\n", sizeof(k));
-	printf("Size of a float: %f.\n", sizeof(f));
+	printf("Size of a char: %ld byte(s)\n", sizeof(c));
+	printf("Size of an int: %ld byte(s)\n", sizeof(i));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(l));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(k));
+	printf("Size of a float: %ld byte(s)\n", sizeof(f));
 
 	return (0);
 }
