90c5953017de43864633a027737da37c915264d7
Valerie Nierenberg <valerie.nierenberg@gmail.com>
2020-09-16 20:31:24 +0000

    Adds 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+/**
+* main- void
+* description: prints with proper grammar, but the
+* outcome is a piece of art,
+* Return: 0
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
