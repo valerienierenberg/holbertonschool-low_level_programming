e8141471c99585def32f4237a1736687ce8b2949
Valerie Nierenberg <valerie.nierenberg@gmail.com>
2020-09-17 20:05:09 +0000

    Add 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+/**
+* main- void
+* description: print 0-9, using putchar, but not char type
+* Return: 0
+*/
+
+#include <stdio.h>
+
+int main(void)
+
+{
+	int a;
+
+	for (a = 0 ; a < 10 ; a++)
+
+		putchar("%d", a);
+
+	putchar("\n");
+
+	return (0);
+}
