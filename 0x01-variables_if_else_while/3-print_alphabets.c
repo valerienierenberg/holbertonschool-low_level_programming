de2c6c24d9155108c6cc716376e280229139f69b
Valerie Nierenberg <valerie.nierenberg@gmail.com>
2020-09-17 19:24:56 +0000

    Add 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+/**
+* main- void
+* description: print a-z, then A-Z
+* Return: 0
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+	putchar(c);
+	}
+	{
+	 putchar('\n');
+	}
+
+	for (c = 'A' ; c <= 'Z' ; c++)
+	{
+	putchar(c);
+	}
+	{
+	 putchar('\n');
+	}
+
+	return (0);
+}
