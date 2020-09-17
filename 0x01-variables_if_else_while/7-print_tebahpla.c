791616c1c8eca7edfc827fbd2baba165c81b2166
Valerie Nierenberg <valerie.nierenberg@gmail.com>
2020-09-17 20:23:09 +0000

    Add 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+/**
+* main- void
+* description: Prints alphabet
+* Return: 0
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	char c;
+
+	for (c = 'z' ; c <= 'a' ; c--)
+	{
+	putchar(c);
+	}
+	{
+	 putchar('\n');
+	}
+
+	return (0);
+}
