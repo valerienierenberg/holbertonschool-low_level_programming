0a0574191f1a500e6b837ddcd255f39dc4413059
Valerie Nierenberg <valerie.nierenberg@gmail.com>
2020-09-17 17:45:58 +0000

    Add 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+/**
+* main- void
+* description: Prints alphabet
+* Return: 0
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+for (int x = 'A'; x <= 'Z'; x++)
+{
+	putchar(tolower(x));
+}
+return (0);
+
+}
