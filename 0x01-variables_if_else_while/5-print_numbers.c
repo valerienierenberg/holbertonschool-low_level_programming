da85556b4071262d13debdabe8f524c07c77991f
Valerie Nierenberg <valerie.nierenberg@gmail.com>
2020-09-17 19:40:36 +0000

    Add 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+/**
+* main- void
+* description: print num 1-10
+* Return: 0
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 0
+
+	while (a < 10)
+	{
+		printf("%d\n", a);
+		a++;
+	}
+	return (0);
+}
