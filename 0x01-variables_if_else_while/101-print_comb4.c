2edee5a033daa315095c8e3830f16ed0745efc7f
Valerie Nierenberg <valerie.nierenberg@gmail.com>
2020-09-18 00:57:48 +0000

    Add 101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+/**
+* main- void
+* description- prints combos of two digits, not repeating digit combos
+* Return: 0
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (a = 0 ; a < 9 ; a++)
+{
+		for (a = b + 1; b < 10 ; b++)
+		{
+			putchar((a % 10) + '0');
+			putchar((b % 10) + '0');
+
+			if (a == 8 && b == 9)
+				continue;
+
+			putchar(',');
+
+			putchar(' ');
+		}
+}
+		putchar('\n');
+
+		return (0);
+}
