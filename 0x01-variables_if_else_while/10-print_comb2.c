03fe6936e3095c1c9e690387b2105daa9eda59ab
Valerie Nierenberg <valerie.nierenberg@gmail.com>
2020-09-19 01:03:33 +0000

    Change to not use mod operator for 10-print_comb2.c

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -11,22 +11,22 @@ int main(void)
 	int a;
 	int b;
 
-	for (a = 0 ; a < 10 ; a++)
-{
-		for (b = 0; b < 10 ; b++)
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = '0'; b <= '9'; b++)
 		{
-			putchar((a % 10) + '0');
-			putchar((b % 10) + '0');
+			putchar(a);
+			putchar(b);
 
-			if (a == 9 && b == 9)
+			if (a == '9' && b == '9')
 				continue;
 
 			putchar(',');
-
 			putchar(' ');
 		}
-}
+	}
+
 		putchar('\n');
 
-		return (0);
+return (0);
 }
