c3ee5068236c46323740d3b0ef5ba36bfcc4f2d5
Valerie Nierenberg <valerie.nierenberg@gmail.com>
2020-09-19 01:51:33 +0000

    Add 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,25 +13,24 @@ int main(void)
 
 	for (a = '0'; a <= '9'; a++)
 	{
-		for (b = '0'; b <= '9'; b++)
+		for (b = a + 1; b <= '9'; b++)
 		{
+			putchar(a);
+			putchar(b);
 
 			if (a == '8' && b == '9')
-				continue;
+			{
+				putchar('\n');
+			}
 
-			if (a == b)
-				break;
-
-				{
-				putchar(a);
-				putchar(b);
+			else
+			{
 				putchar(',');
 				putchar(' ');
-				}
+			}
 		}
 	}
 
-		putchar('\n');
 
 return (0);
 }
