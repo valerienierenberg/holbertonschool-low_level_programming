7a3df5a8d0769de9aecc5d43d216da97a2b7d9cf
Valerie Nierenberg <valerie.nierenberg@gmail.com>
2020-09-20 22:32:23 +0000

    Remove extra parens from 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,10 +15,10 @@ int main(void)
 	{
 		for (b = a + 1; b <= 99; b++)
 		{
-			putchar((a / 10) % 10 + '0');
+			putchar(a / 10 % 10 + '0');
 			putchar(a % 10 + '0');
 			putchar(' ');
-			putchar((b / 10 % 10 + '0');
+			putchar(b / 10 % 10 + '0');
 			putchar(b % 10 + '0');
 		}
 			if (a == 98 && b == 99)
