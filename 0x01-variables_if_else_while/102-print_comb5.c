8698ca46a750e0b53cbb8f980dcb854b4461312a
Valerie Nierenberg <valerie.nierenberg@gmail.com>
2020-09-20 22:30:37 +0000

    Add 102-printcomb5.c with use of modulus operator

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,22 +11,27 @@ int main(void)
 	int a;
 	int b;
 
-	for (a = '0'; a <= '9'; a++)
+	for (a = 00; a <= 98; a++)
 	{
-		for (b = '0'; b <= '9'; b++)
+		for (b = a + 1; b <= 99; b++)
 		{
-			putchar(a);
-			putchar(b);
+			putchar((a / 10) % 10 + '0');
+			putchar(a % 10 + '0');
+			putchar(' ');
+			putchar((b / 10 % 10 + '0');
+			putchar(b % 10 + '0');
+		}
+			if (a == 98 && b == 99)
+			{
+			putchar('\n');
+			}
 
-			if (a == '9' && b == '9')
-				continue;
-
+			else
+			{
 			putchar(',');
 			putchar(' ');
-		}
+			}
 	}
-
-		putchar('\n');
 
 return (0);
 }
