5729e7c19d89493348d06b5b8a069975533e659d
Valerie Nierenberg <valerie.nierenberg@gmail.com>
2020-09-21 20:44:53 +0000

    Adds 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+/**
+* print_alphabet_x10- void
+* description: prints alphabet ten times followed by newline
+* return: 0
+*/
+
+#include "holberton.h"
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+		_putchar(c);
+		}
+	
+	_putchar('\n');
+	}
+
+}
