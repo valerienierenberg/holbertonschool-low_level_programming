c3680d91297473ed958836d63598dc7173ca9737
Valerie Nierenberg <valerie.nierenberg@gmail.com>
2020-09-21 21:37:38 +0000

    Add 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+/**
+* print_sign- prints the sign of a number
+* description: alpha characters
+* Return: 0, 1, or -1
+*/
+
+#include "holberton.h"
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+	_putchar('+');
+	return (1);
+	}
+
+	else if (n < 0)
+	{
+	_putchar('-');
+	return (-1);
+	}
+
+	else
+	{
+	_putchar('0');
+	return (0);
+	}
+
+}
