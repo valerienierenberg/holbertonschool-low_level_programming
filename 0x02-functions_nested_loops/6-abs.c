7a9a9d12c14a019c97c072c4baff92f43882b905
Valerie Nierenberg <valerie.nierenberg@gmail.com>
2020-09-22 00:24:08 +0000

    Adds 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+/**
+* _abs- finds absolute value
+* description: prints absolute value of number
+* Return: c, -c
+*/
+
+#include "holberton.h"
+
+int _abs(int c)
+{
+
+	if (c >= 0)
+		return (c);
+
+	else
+		return (-c);
+}
