04ca71be863a4274c8651069505a99170348a306
Valerie Nierenberg <valerie.nierenberg@gmail.com>
2020-09-22 16:14:19 +0000

    Change while to for statements

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,16 +11,16 @@ void print_to_98(int n)
 {
 	if (n >= 98)
 	{
-		while (n > 98)
-			printf("%d, ", n--);
+		for (; n > 98; n--)
+			printf("%d, ", n);
 		printf("%d\n", n);
 	}
 
 
 	else
 	{
-		while (n < 98)
-			printf("%d, ", n++);
+		for (; n < 98; n++)
+			printf("%d, ", n);
 		printf("%d\n", n);
 	}
 }
