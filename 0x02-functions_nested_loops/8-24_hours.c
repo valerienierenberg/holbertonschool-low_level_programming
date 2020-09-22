612ffefe15713788713749963a351d4f708e0de3
Valerie Nierenberg <valerie.nierenberg@gmail.com>
2020-09-22 01:18:49 +0000

    Add 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+/**
+* jack_bauer- prints every minute of day
+* description: prints the time every minute of day
+* Return: void
+*/
+
+#include "holberton.h"
+
+void jack_bauer(void)
+{
+	int a;
+	int b;
+
+	for (a = 00; a <= 23; a++)
+	{
+		for (b = 00; b <= 59; b++)
+		{
+			_putchar(a / 10 % 10 + '0');
+			_putchar(a % 10 + '0');
+			_putchar(':');
+			_putchar(b / 10 % 10 + '0');
+			_putchar(b % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
