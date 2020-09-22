874276de0458360c23775bd50e5365ed74977d7d
Valerie Nierenberg <valerie.nierenberg@gmail.com>
2020-09-22 14:31:55 +0000

    Amends comment section for betty task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,17 @@
 /**
-* _islower- checks for lowercase
-* description: checks for lower case
-* Return: 0 or 1
+* _islower -  checks for lowercase character.
+* @c: lowercase a-z
+*
+* Description: Returns 1 if c is lowercase, else 0
+* section header: Section description
+* Return: returns int
 */
 
 #include "holberton.h"
 
 int _islower(int c)
 {
+
 	if (c >= 97 && c <= 122)
 	{
 		return (1);
