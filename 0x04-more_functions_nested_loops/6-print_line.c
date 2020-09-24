fd6b711e9e67f51b833695a26dfcf53695017eba
Valerie Nierenberg <valerie.nierenberg@gmail.com>
2020-09-24 18:51:11 +0000

    Adds 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+/**
+* print_line- void
+* @n: number
+* description: prints a line on the terminal
+* return: void
+*/
+
+#include "holberton.h"
+
+void print_line(int n)
+{
+	int line;
+
+	if (n > 0)
+	{
+	for (line = 0; line < n; line++)
+		_putchar('_');
+	}
+		_putchar('\n');
+}
