7f12235917aa6844249cb3712a20987cf7091d3c
Valerie Nierenberg <valerie.nierenberg@gmail.com>
2020-09-24 20:33:07 +0000

    Add 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -18,15 +18,16 @@ void print_square(int size)
 		for (height = 0; height < size; height++)
 		{
 			for (width = 0; width < size; width++)
-
+			{
 			_putchar('#');
+			}
 
-			if (height == size - 1)
-				continue;
+		_putchar('\n');
 
+			if (size <= 0)
+			{
 			_putchar('\n');
+			}
 		}
 	}
-
-_putchar('\n');
 }
