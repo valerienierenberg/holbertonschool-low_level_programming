9be3001039cd310bfeb69bd56bdbea0c7c2fde83
Valerie Nierenberg <valerie.nierenberg@gmail.com>
2020-09-24 14:34:09 +0000

    Add 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+/**
+* _isdigit -  checks for digit
+* @c: digit
+*
+* Description: Returns 1 if c is digit 0-9, else return 0
+* section header: Section description
+* Return: returns int
+*/
+
+#include "holberton.h"
+
+int mul(int a, int b);
+{
+		return (a * b);
+}
