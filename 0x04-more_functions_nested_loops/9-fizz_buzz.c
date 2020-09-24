6d9500576d72f3d198944bd8bee18205398261f1
Valerie Nierenberg <valerie.nierenberg@gmail.com>
2020-09-24 21:00:51 +0000

    Add 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+/**
+* main- prints 1-100, multiples of 3 print Fizz
+* Multiples of 5 print Buzz, mult of 3 and 5 print fizzbuzz
+* @void: number
+* description: prints a line on the terminal
+* Return:0
+*/
+
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if (num % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+
+		if (num % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+
+		if ((num % 3 != 0) && (num % 5 != 0))
+		{
+			printf("%d", num);
+		}
+		if (num != 100)
+
+			printf(" ");
+	}
+
+	putchar('\n');
+	return (0);
+}
