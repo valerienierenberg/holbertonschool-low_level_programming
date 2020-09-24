82783cc801dfa4cfd10ed8efb5faaa9bb7e3828f
Valerie Nierenberg <valerie.nierenberg@gmail.com>
2020-09-24 19:37:14 +0000

    Add 8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,32 @@
+/**
+* print_square- prints square of # symbols
+* @size: number
+* description: prints a square on the terminal
+* return:void
+*/
+
+#include "holberton.h"
+
+void print_square(int size)
+{
+	int height;
+	int width;
+
+	if (size > 0)
+	{
+
+		for (height = 0; height < size; height++)
+		{
+			for (width = 0; width < size; width++)
+
+			_putchar('#');
+
+			if (height == size - 1)
+				continue;
+
+			_putchar('\n');
+		}
+	}
+
+_putchar('\n');
+}
