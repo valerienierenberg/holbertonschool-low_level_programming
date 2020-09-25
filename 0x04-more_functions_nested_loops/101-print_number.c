dddfa46a05518965289022ecde4ad3e6475f4d38
Valerie Nierenberg <valerie.nierenberg@gmail.com>
2020-09-25 20:35:52 +0000

    Add 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,12 +1,40 @@
 /**
 * print_number- prints integer
 * @n: any number
-* Return: c, -c
+* Return: 0
 */
 
 #include "holberton.h"
 
 void print_number(int n)
 {
-	_putchar(n)
+	char a;
+	int b;
+	char c;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		a = ('0' - (n % 10));
+		n /= -10;
+	}
+	else
+	{
+		a = ((n % 10) + '0');
+		n /= 10;
+	}
+	b = 0;
+	while (n > 0)
+	{
+		b = b * 10 + (n % 10);
+		n /= 10;
+	}
+
+	while (b > 0)
+	{
+		c = ((b % 10) + '0');
+		_putchar(c);
+		b /= 10;
+	}
+	_putchar(a);
 }
