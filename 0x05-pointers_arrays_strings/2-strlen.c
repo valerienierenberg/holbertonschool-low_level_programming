9cbd32445e3d9bc9ebe4bddf6f3da6ba392e5fc9
Valerie Nierenberg <valerie.nierenberg@gmail.com>
2020-09-28 19:58:05 +0000

    Add all

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+* _strlen -  returns the length of a string
+* @s: string
+*
+* Description: returns the length of a string
+* Return: ln
+*/
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+
+	return (i);
