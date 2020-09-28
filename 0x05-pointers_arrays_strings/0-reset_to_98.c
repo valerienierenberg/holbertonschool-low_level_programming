32f4e4fa4326e1b812c74b6b6eb307861743c183
Valerie Nierenberg <valerie.nierenberg@gmail.com>
2020-09-28 15:37:41 +0000

    Add 0-reset_to_98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,3 +1,14 @@
-
+/**
+* reset_to_98 -  updates value the pointer *n points to
+* to be 98.
+* @n: number
+*
+* Description: update value the pointer *n points to
+* to be 98.
+* Return: void
+*/
 
 void reset_to_98(int *n)
+{
+	*n = 98;
+}
