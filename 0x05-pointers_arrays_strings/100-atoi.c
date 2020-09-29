6a186c0c53964da6daa1e929eb0b2d2847571d9f
Valerie Nierenberg <valerie.nierenberg@gmail.com>
2020-09-29 19:05:37 +0000

    Add 100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+* rev_string - converts a string to an int
+* @s: string
+*
+* Description: converts string to int
+* Return: int
+*/
+
+int _atoi(char *s)
+{
+    int digit;
+    int j = 1;
+    int m = 0;
+
+    for (int i = strlen(s) -1; i >= 0; i--)
+    {
+        digit = s[i];
+        digit = digit - 48;
+        m = m + (digit * j);
+        j = j * 10;
+    }
+    return (m);
+}
