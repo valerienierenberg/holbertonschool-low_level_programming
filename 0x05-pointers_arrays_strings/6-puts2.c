2a164b50311b262c0eabc90c4c5277f50e7d1818
Valerie Nierenberg <valerie.nierenberg@gmail.com>
2020-09-29 14:57:12 +0000

    Adds 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,9 +12,16 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i += 2)
-		_putchar(str[i]);
+	i = 0;
 
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+
+		i++;
+	}
 	_putchar('\n');
-
 }
