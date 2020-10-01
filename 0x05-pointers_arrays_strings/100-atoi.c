5bea17d03ef68be26960d83d6762bd2f426b2386
Valerie Nierenberg <valerie.nierenberg@gmail.com>
2020-10-01 00:11:54 +0000

    Add atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,24 +8,26 @@
 * Description: converts a string to an int
 * Return: void
 */
-
 int _atoi(char *s)
 {
-	int res;
-	int i;
-
-	res = 0;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (i < 0)
-		{
-			i *= -1;
-			_putchar('-');
-		}
-
-	res = res * 10 + s[i] - '0';
-	}
-
-	return (res);
+short boo;
+int i, min, res;
+i = min = res = boo = 0;
+min = -1;
+while (s[i] != '\0')
+{
+if (s[i] == '-')
+min *= -1;
+if (s[i] >= '0' && s[i] <= '9')
+{
+res *= 10;
+res -= (s[i] - '0');
+boo = 1;
+}
+else if (boo == 1)
+break;
+i++;
+}
+res *= min;
+return (res);
 }
