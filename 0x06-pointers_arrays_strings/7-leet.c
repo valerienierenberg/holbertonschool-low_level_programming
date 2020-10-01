de1a1eeb8fdbb4bfc63548d0aef070b36acdd0de
Valerie Nierenberg <valerie.nierenberg@gmail.com>
2020-10-01 21:59:45 +0000

    Add 7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * leet - converts a string to leet
+ * @s: string
+ *
+ * Return: s
+ */
+char *leet(char *s)
+{
+	int i, j;
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (a[j] == s[i])
+				s[i] = b[j];
+		}
+	}
+return (s);
+}
