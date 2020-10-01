ac1cafd9df72e76bfca99d1ecc2bb6eacadadfbe
Valerie Nierenberg <valerie.nierenberg@gmail.com>
2020-10-01 15:44:09 +0000

    Add 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,23 +9,23 @@
 */
 char *_strncat(char *dest, char *src, int n)
 {
-int i, j;
+	int i, j;
 
-i = 0;
-j = 0;
+	i = 0;
+	j = 0;
 
-while (*(dest + i))
-i++;
+	while (*(dest + i))
+		i++;
 
-while (j < n && *(src + j))
-{
-*(dest + i) = *(src + j);
-i++;
-j++;
-}
+	while (j < n && *(src + j))
+	{
+		*(dest + i) = *(src + j);
+		i++;
+		j++;
+	}
 
-if (j < n)
-*(dest + i) = *(src + j);
+	if (j < n)
+		*(dest + i) = *(src + j);
 
 return (dest);
 }
