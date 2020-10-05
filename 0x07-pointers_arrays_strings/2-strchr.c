4ba7eef8c23e4b1d23b54b274cff7dd3e7419c29
Valerie Nierenberg <valerie.nierenberg@gmail.com>
2020-10-05 16:11:10 +0000

    Adds 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+* _strchr - locates a character in a string
+* @s: string to search through for char c
+* @c: character to search for
+*
+* Return: A pointer to the first
+* occurrence of the char c in the string s, or NULL
+* if char is not found
+*/
+char *_strchr(char *s, char c)
+{
+
+for (;; s++)
+{
+	if (*s == c)
+		return (s);
+	if (!*s)
+		return ("NULL");
+}
+return ("NULL");
+}
