fcee9b01e3c94bf3126a10d36dd27e23e9f8447a
Valerie Nierenberg <valerie.nierenberg@gmail.com>
2020-10-05 19:28:38 +0000

    Changes 2-strchr.c to while

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,14 +10,17 @@
 */
 char *_strchr(char *s, char c)
 {
-int i;
+int i = 0;
 
-for (i = 0; s[i] >= '\0'; i++)
+while (s[i])
 {
 if (s[i] == c)
 return (s + i);
+i++;
 }
+
+if (s[i] == c)
+return (s + i);
 
 return ('\0');
 }
-
