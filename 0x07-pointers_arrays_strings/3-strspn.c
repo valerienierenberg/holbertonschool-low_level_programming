ec0f13f075acf4e2bff5309bd949efa6e5c82411
Valerie Nierenberg <valerie.nierenberg@gmail.com>
2020-10-05 18:47:24 +0000

    Adds 3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+* _strspn - gets length of a prefix substring
+* @s: string
+* @accept: string to compare to s and see what matches
+*
+* Return: number of bytes of s in accpet
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int i, j;
+
+for (i = 0; s[i]; i++)
+{
+	for (j = 0; accept[j]; j++)
+	{
+		if (s[i] == accept[j])
+		break;
+	}
+
+	if (!accept[j])
+		break;
+}
+return (i);
+}
+
