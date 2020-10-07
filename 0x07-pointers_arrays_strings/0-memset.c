93d51e8efa450f9e5c8b1633f412bfbdd9347b75
Valerie Nierenberg <valerie.nierenberg@gmail.com>
2020-10-07 14:34:00 +0000

    Modifies 0-memset.c and adds descriptive comments

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,25 +1,32 @@
 #include "holberton.h"
+
 /**
-* _memset - Fills the first @n bytes of the memory area
-*          pointed to by @s with the constant byte @b.
-* Fills memory with a constant byte.
-* @s: A pointer to the memory area to be filled.
-* @b: The character to fill the memory area with.
-* @n: The number of bytes to be filled.
-*
+* _memset - Function that fills memory with a constant byte.
+* @s: pointer to memory area to be filled.
+* @b: character to fill memory area.
+* @n: number of bytes to be filled.
 * Return: A pointer to the filled memory area @s.
 */
+
 char *_memset(char *s, char b, unsigned int n)
 {
-char *p = s;  /* set pointer p to value s */
+    unsigned int i; /* changed to unsigned int bc it must be same as n */
 
-while (n > 0)  /* while the number of bytes to
-* be filled in memory is more than 0
+    for (i = 0; i < n; i++)
+    {
+    *(s + i) = b; /* s (NOT *s) is address pointer is pointing to */
+    }   /* *(s + i) shifts memory over 1 each time i is incremented */
+return (s);
+}
+/*
+* Original code below works for checker as well
+* char *p = s;
+*
+* while (n > 0)
+* {
+* *p = b;
+* p++;
+* n--;
+* }
+* return (s);
 */
-{
-*p = b;  /* dereference pointer (set value at p to b) */
-p++;    /* increment value at p (incrementing b??)*/
-n--;   /* decrement number of bytes to be filled */
-}
-return (s); /* return pointer to memory area s */
-}
