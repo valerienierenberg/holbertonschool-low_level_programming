e5bbad8143f07fd8ff82b55b8786e03e8c3b96c4
Valerie Nierenberg <valerie.nierenberg@gmail.com>
2020-10-07 14:34:16 +0000

    Adds 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
-* _memcpy - copies memory area
+* _memcpy - Function that copies memory area.
 * @dest: memory area for source to be copied to
 * @src: memory are that n bytes are copied from
 * @n: The number of bytes to be copied.
@@ -9,14 +9,24 @@
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-/* Typecast src and dest addresses to (char *) */
-char *csrc = (char *)src;
-char *cdest = (char *)dest;
-unsigned int i;
+    unsigned int mem; /* mem = memory */
 
-/* Copy contents of src[] to dest[] */
-for (i = 0; i < n; i++)
-cdest[i] = csrc[i];
-
+    for (mem = 0; mem < n; mem++)
+    {
+        *(dest + mem) = *(src + mem);
+    }
 return (dest);
 }
+/*
+* First code and comments below. Code is below COMMENT>
+* COMMENT> Typecast src and dest addresses to (char *)
+* char *csrc = (char *)src;
+* char *cdest = (char *)dest;
+* unsigned int i;
+*
+* COMMENT> Copy contents of src[] to dest[]
+* for (i = 0; i < n; i++)
+* cdest[i] = csrc[i];
+*
+*return (dest);
+*/
