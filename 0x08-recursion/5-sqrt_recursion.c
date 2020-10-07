507226b7826115197fcfdebbcccd166166352460
Valerie Nierenberg <valerie.nierenberg@gmail.com>
2020-10-07 15:02:40 +0000

    Adds 5-sqrt_recursion.c with betty compliance

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -16,8 +16,8 @@ int check_sqrt(int a, int c)
 
 int _sqrt_recursion(int n)
 {
-    if (n == 0)
-        return (0);
-	return (check_sqrt(1, n));
+	if (n == 0)
+		return (0);
+		return (check_sqrt(1, n));
 }
 
