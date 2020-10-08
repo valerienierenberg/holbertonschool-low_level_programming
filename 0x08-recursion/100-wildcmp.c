91f6bf011a02c58d877687e021e546711e4eaf8b
Valerie Nierenberg <valerie.nierenberg@gmail.com>
2020-10-08 00:57:52 +0000

    Adds 100-wildcmp.c

---
+++ 0x08-recursion/100-wildcmp.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+* wildcmp - compares two strings to see if identical
+* (s2 can contain *, * can replace any string,
+* including an empty string)
+* @s1: string to compare
+* @s2: string to compare
+* Return: 1 if identical, otherwise 0
+*/
+int wildcmp(char *s1, char *s2)
+{
+	return (1);
+}
