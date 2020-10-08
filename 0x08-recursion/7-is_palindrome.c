4f55fb0d966515c49465ecd4f7cf61db467fdc15
Valerie Nierenberg <valerie.nierenberg@gmail.com>
2020-10-08 00:54:18 +0000

    Adds empty 7-is_palindrome.c

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -7,23 +7,4 @@
 */
 int is_palindrome(char *s)
 {
-if (i < 2 || i % f == 0)
-	return (0);
-else if (f > i / 2)
-	return (1);
-else
-	return (check_prime(f + 1, i));
-}
-
-/**
-* is_prime_number - returns 1 if n is prime
-* @n: number to check if prime
-* Return: 1 if n is prime, otherwise 0
-*/
-int is_prime_number(int n)
-{
-/* Base case */
-if (n == 2)
-	return (1);
-	return (check_prime(2, n));
 }
