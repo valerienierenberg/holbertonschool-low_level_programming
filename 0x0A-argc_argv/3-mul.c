800e6c5180da18bcfe6cb066413ccf21fee9c083
Valerie Nierenberg <valerie.nierenberg@gmail.com>
2020-10-10 13:18:27 +0000

    Adds 3-mul.c with Error printing instead of Seg fault

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main- prints arguments
+* @argc: number of arguments
+* @argv: names of arguments
+* description: prints arguments
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	int prod;
+	int num1;
+	int num2;
+/* This line is giving me segmentation fault instead of printing error */
+	if (argc < 3)
+		{
+		printf("Error\n");
+		return(1);
+		}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	prod = num1 * num2;
+	printf("%d\n", prod);
+
+return (0);
+}
