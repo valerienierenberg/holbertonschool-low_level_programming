21efe69fde0e0c1ffae5b17670132d2a0b4fb0c9
Valerie Nierenberg <valerie.nierenberg@gmail.com>
2020-10-14 19:43:34 +0000

    Adds 5-argstostr.c

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -3,8 +3,8 @@
 #include <stdlib.h>
 /**
 * *argstostr- concatenates all arguments of program
-* @ac: argument count
-* @av: argument vector
+* @ac: number of args
+* @av: array of arguments
 * Return: NULL if ac == 0 or av == NULL
 * NULL also if function fails
 * Otherwise returns a pointer to a new string
@@ -12,10 +12,35 @@
 
 char *argstostr(int ac, char **av)
 {
-char *s;
+char *ptr;
+int i, j;
+int buffer = 0;
+int len = 0;
 
-if (ac == 0 || av == NULL)
+if (ac == 0)
 return (NULL);
 
-return (s);
+if (av == NULL)
+return (NULL);
+
+for (i = 0; av[i]; i++)
+
+	for (j = 0; av[i][j]; j++)
+		len++;                  /* gets length of every char in arguments */
+
+ptr = (char *)malloc(len * sizeof(char) + ac + 1);
+if (!ptr)
+	return (NULL);
+
+
+for (i = 0; av[i]; i++)
+{
+	for (j = 0; av[i][j]; j++, buffer++)
+		ptr[buffer] = av[i][j];
+
+	ptr[buffer] = '\n';
+		buffer++;
+}
+ptr[buffer] = '\0';
+return (ptr);
 }
