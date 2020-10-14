31caa210c45b344905bdc9bc01ba16905a3a8890
Valerie Nierenberg <valerie.nierenberg@gmail.com>
2020-10-14 21:53:42 +0000

    Adds 100-strtow.c

--- 0x0B-malloc_free/100-strtow.c
+++ 0x0B-malloc_free/100-strtow.c
@@ -1,20 +1,77 @@
-#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
 /**
-* strtow- splits a string into words
-* @str: string to split
-* Return: NULL if str == NULL or str == ""
-* NULL also if function fails
-* Otherwise returns a pointer to an array of strings (words)
-*/
-
+ * strtow - split a string into words
+ *
+ * @str: string to be split
+ *
+ * Return: POINTER to a new array of "words"
+ * OR NULL if str is NULL or empty
+ * OR NULL if fail
+ */
 char **strtow(char *str)
 {
-char **s;
-
-if (str == NULL || str == "")
-return (NULL);
-
-return (s);
+ char **new;
+        int wrd;
+        int sz = 0;
+        int i = 0;
+        int a = 0;
+        int b = 0;
+        int mem = 0;
+        int plc = 0;
+        if (str == NULL || *str == '\0')
+                return (NULL);
+        /* This check for number of words, not chars */
+        while (str[sz] != '\0')
+        {
+                if (str[sz] == ' ')
+                        sz++;
+                else
+                {
+                        while (str[sz] != ' ' && str[sz] != '\0')
+                                sz++;
+                        wrd++;
+                }
+        }
+        /* sz + 1 is to accomodate for and escape char */
+        new = (char **)malloc((wrd + 1) * sizeof(char *));
+        /* this while loop gets num of chars and allocated mem */
+        while (str[a] != '\0')
+        {
+                if (str[a] == ' ')
+                        a++;
+                else
+                {
+                        b = 0;
+                        while (str[a] != ' ' && str[a] != '\0')
+                        {
+                                a++;
+                                b++;
+                        }
+                        new[mem] = malloc((b + 1) * sizeof(char));
+                        mem++;
+                }
+        }
+        mem = 0;
+        while (str[i] != '\0')
+        {
+                if (str[i] == ' ')
+                        i++;
+                else
+                {
+                        plc = 0;
+                        while (str[i] != ' ' && str[i] != '\0')
+                        {
+                                new[mem][plc] = str[i];
+                                i++;
+                                plc++;
+                        }
+                        new[mem][plc] = '\0';
+                        mem++;
+                }
+        }
+        for (i = 0; new[i] != NULL; i++)
+                printf("new[i] = %p\n", new[i]);
+        new[mem] = NULL;
+        return (new);
 }
