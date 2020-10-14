ad7c8e5bf05ed536802f0f119577244b140983d6
Valerie Nierenberg <valerie.nierenberg@gmail.com>
2020-10-14 00:57:37 +0000

    Adds 5-argstostr.c:

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* *argstostr- concatenates all arguments of program
+* @ac: argument count
+* @av: argument vector
+* Return: NULL if ac == 0 or av == NULL
+* NULL also if function fails
+* Otherwise returns a pointer to a new string
+*/
+
+char *argstostr(int ac, char **av)
+{
+char *s;
+
+if (ac == 0 || av == NULL)
+return (NULL);
+
+return (s);
+}
