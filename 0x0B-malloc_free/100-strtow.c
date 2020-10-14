7312040c49674dcdedb1423486e351310c7145e8
Valerie Nierenberg <valerie.nierenberg@gmail.com>
2020-10-14 01:01:10 +0000

    Adds 100-strtow.c

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* strtow- splits a string into words
+* @str: string to split
+* Return: NULL if str == NULL or str == ""
+* NULL also if function fails
+* Otherwise returns a pointer to an array of strings (words)
+*/
+
+char **strtow(char *str)
+{
+char **s;
+
+if (str == NULL || str == "")
+return (NULL);
+
+return (s);
+}
