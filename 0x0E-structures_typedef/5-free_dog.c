fc27de65d15d89e634e723b8eb093baed53f5df4
Valerie Nierenberg <valerie.nierenberg@gmail.com>
2020-10-19 18:52:28 +0000

    Added 5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* free_dog- Function that frees dogs.
+* @d: pointer char to a structure
+*
+* Return: void
+*/
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
+
