f6b89a726c183e54b6b7eb4fefdbcd344f8c1f6a
Valerie Nierenberg <valerie.nierenberg@gmail.com>
2020-10-19 17:52:04 +0000

    Added dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/**
+ * struct dog-structure with name, age, owner elements
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner's name
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
