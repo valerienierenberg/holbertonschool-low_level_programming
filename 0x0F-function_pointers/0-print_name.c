babff9a45515f0c77f70d414da73f710b6d9f21d
Valerie Nierenberg <valerie.nierenberg@gmail.com>
2020-10-21 15:42:55 +0000

    Adds 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* print_name-prints a name
+* @name: character array
+* @f: function pointer
+* Return: void
+*/
+void print_name(char *name, void (*f)(char *))
+{
+if (name != NULL && f != NULL)
+f(name);
+}
