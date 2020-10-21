f889f6e1afe1a20cd25115789ba23eb2eeee8921
Valerie Nierenberg <valerie.nierenberg@gmail.com>
2020-10-21 16:06:27 +0000

    Adds stddef.h to function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,6 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+#include <stddef.h>
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
@@ -7,4 +8,3 @@ void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
 #endif
-
