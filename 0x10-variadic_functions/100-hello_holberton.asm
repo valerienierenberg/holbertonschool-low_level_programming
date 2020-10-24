706795d881a7e6fd02ca27cf67faa741775c6c7c
valerienierenberg <valerie.nierenberg@gmail.com>
2020-10-24 22:10:49 +0000

    Add 100-hello_holberton.asm

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,22 +1,13 @@
-section .text
-    global _start
-
-section .data
-msg db  'Hello, world!',0xa ;our dear string
-len equ $ - msg
-
-section .text
-
-_start:
-
-
-    mov edx,len
-    mov ecx,msg
-    mov ebx,1
-    mov eax,4
-    int 0x80
-
-
-    mov ebx,0
-    mov eax,1
-    int 0x80
+section	.data
+msg:	 db "Hello, Holberton", 10
+section	.text
+	global main
+main:
+	mov edx, 17
+	mov ecx, msg
+	mov ebx, 1
+	mov eax, 4
+	int 0x80
+	mov ebx, 0
+	mov eax, 1
+	int 0x80
