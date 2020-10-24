f8cfe39ae52f329d85453dfb6fe1e4f41309e5a4
valerienierenberg <valerie.nierenberg@gmail.com>
2020-10-24 18:16:05 +0000

    Add 100-hello_holbeton.asm

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,15 +1,29 @@
+        global  go
+        extern  _ExitProcess@4
+        extern  _GetStdHandle@4
+        extern  _WriteConsoleA@20
 
-          global    _start
+        section .data
+msg:    db      'Hello, Holberton', 10
+handle: db      0
+written:
+        db      0
 
-          section   .text
-_start:   mov       rax, 1
-          mov       rdi, 1
-          mov       rsi, message
-          mov       rdx, 13
-          syscall
-          mov       rax, 60
-          xor       rdi, rdi
-          syscall
+        section .text
+go:
+        ; handle = GetStdHandle(-11)
+        push    dword -11
+        call    _GetStdHandle@4
+        mov     [handle], eax
 
-          section   .data
-message:  db        "Hello, Holberton", 10
+        ; WriteConsole(handle, &msg[0], 13, &written, 0)
+        push    dword 0
+        push    written
+        push    dword 13
+        push    msg
+        push    dword [handle]
+        call    _WriteConsoleA@20
+
+        ; ExitProcess(0)
+        push    dword 0
+        call    _ExitProcess@4
