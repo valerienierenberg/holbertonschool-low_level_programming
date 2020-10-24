8cef469a5f6621e6f260b49bfcf5d825f8d14b58
valerienierenberg <valerie.nierenberg@gmail.com>
2020-10-24 18:13:00 +0000

    Added 100-hello_holberton.asm

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -2,14 +2,14 @@
           global    _start
 
           section   .text
-_start:   mov       rax, 1                  ; system call for write
-          mov       rdi, 1                  ; file handle 1 is stdout
-          mov       rsi, message            ; address of string to output
-          mov       rdx, 13                 ; number of bytes
-          syscall                           ; invoke operating system to do the write
-          mov       rax, 60                 ; system call for exit
-          xor       rdi, rdi                ; exit code 0
-          syscall                           ; invoke operating system to exit
+_start:   mov       rax, 1
+          mov       rdi, 1
+          mov       rsi, message
+          mov       rdx, 13
+          syscall
+          mov       rax, 60
+          xor       rdi, rdi
+          syscall
 
           section   .data
-message:  db        "Hello, World", 10      ; note the newline at the end
+message:  db        "Hello, Holberton", 10
