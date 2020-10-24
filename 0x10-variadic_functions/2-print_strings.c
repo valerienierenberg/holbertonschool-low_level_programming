e217a718e14a13d65e17da12019412d3f35f27f7
valerienierenberg <valerie.nierenberg@gmail.com>
2020-10-24 16:48:31 +0000

    Move if statement

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -17,10 +17,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 va_list args;
 unsigned int i;
 
-va_start(args, n);
-
 if (args == NULL)
 printf("(nil)\n");
+
+va_start(args, n);
 
 for (i = 0; i < n; i++)
 {
