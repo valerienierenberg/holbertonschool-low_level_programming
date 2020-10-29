a6a555421e6f8a4f969729e7400b36abebfa71fe
valerienierenberg <valerie.nierenberg@gmail.com>
2020-10-29 13:39:13 +0000

    Add 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,8 +9,24 @@
 */
 size_t print_list(const list_t *h)
 {
+	size_t n = 0;
 
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] %p\n", h->str);
+			h = h->next;
+			n++;
+		}
 
-
-
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+			h = h->next;
+			n++;
+		}
+	}
+return (n);
 }
+
