d22142501b24394829a272fdb36bda2897f72607
valerienierenberg <valerie.nierenberg@gmail.com>
2020-10-29 23:58:52 +0000

    Adds 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,2 +1,21 @@
 #include <stdio.h>
 #include "lists.h"
+
+/**
+* free_list - Function that frees a list_t list.
+* @head: pointer
+*/
+
+void free_list(list_t *head)
+{
+	list_t *tmp;
+	list_t *prev = head;
+
+	while (prev != NULL)
+	{
+		tmp = prev->next;
+		free(prev->str);
+		free(prev);
+		prev = tmp;
+	}
+}
