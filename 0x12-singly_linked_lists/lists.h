bc457db8b2d541af77cb899a51422be2d5dbf097
valerienierenberg <valerie.nierenberg@gmail.com>
2020-10-29 13:43:55 +0000

    Update header file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,14 +1,10 @@
 #ifndef LISTS_H
 #define LISTS_H
+
 #include <stdio.h>
-
-int _putchar(char c);
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
-
+#include <stddef.h>
+#include <string.h>
+#include <stdlib.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -25,6 +21,13 @@ typedef struct list_s
     struct list_s *next;
 } list_t;
 
+
+int _putchar(char c);
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
 #endif
 
