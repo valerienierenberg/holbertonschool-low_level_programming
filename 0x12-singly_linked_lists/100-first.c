05c66320a9b744de984f68521962e366bf05a266
valerienierenberg <valerie.nierenberg@gmail.com>
2020-10-29 23:59:28 +0000

    Add 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,2 +1,14 @@
 #include <stdio.h>
 #include "lists.h"
+
+/**
+* print_b4_main- Function that prints
+* "You're beat! and yet, you must allow, I bore my house upon my back!"
+* before the main function is executed
+*/
+
+void print_b4_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
