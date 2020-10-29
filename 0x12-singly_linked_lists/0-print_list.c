e6963f85e3a7157e9f16527a6195302fb4e8d937
valerienierenberg <valerie.nierenberg@gmail.com>
2020-10-29 13:17:24 +0000

    Add header file with prototypes, README.md with function descriptions, and all empty task files

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+/**
+* print_list- prints all the elements of a list_t list
+* @h: pointer (list of type list_t)
+*
+* Return: the number of nodes in the list (type size_t)
+*/
+size_t print_list(const list_t *h)
+{
+
+
+
+
+}
