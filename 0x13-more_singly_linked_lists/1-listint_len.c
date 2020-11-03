ea02db81a53da576099b1087c0a31e41b9eeac3a
valerienierenberg <valerie.nierenberg@gmail.com>
2020-11-03 15:16:52 +0000

    Add comment to 1-listint_len.c with function description and return description

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -8,8 +8,8 @@
 #include <stdlib.h>
 #include <string.h>
 /**
-* list_len- returns the number of elements in a linked list_t list
-* @h: pointer (list of type list_t)
+* listint_len- returns the number of elements in a linked listint_t list
+* @h: pointer (list of type listint_t)
 *
 * Return: the number of elements in a linked list (type size_t)
 */
