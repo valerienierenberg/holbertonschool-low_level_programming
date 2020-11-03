5cc87dc4cc99f319f399ddef7c156b6432ec6a6b
valerienierenberg <valerie.nierenberg@gmail.com>
2020-11-03 17:19:09 +0000

    Please Betty

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,32 +9,33 @@
 * Return: n (data)
 */
 
-int pop_listint(listint_t **head) 
+int pop_listint(listint_t **head)
 {
-    listint_t *tmp = *head, *prev;
-    int key = 0;
-    int n = 0;
- 
-    if (head == NULL)
-        return (0);
+	listint_t *tmp = *head, *prev;
+	int key = 0;
+	int n = 0;
 
-    if (tmp != NULL && tmp->n == key)
-    { 
-        *head = tmp->next;
-        free(tmp);
-        return (n);
-    } 
-  
-    while (tmp != NULL && tmp->n != key)
-    {
-        prev = tmp;
-        tmp = tmp->next;
-    }
+	if (head == NULL)
+		return (0);
 
-    if (tmp == NULL)
-        return (n);
+	if (tmp != NULL && tmp->n == key)
+	{
+		*head = tmp->next;
+		free(tmp);
+		return (n);
+	}
 
-    prev->next = tmp->next;
-    free(tmp);
-    return (n);
+	while (tmp != NULL && tmp->n != key)
+	{
+		prev = tmp;
+		tmp = tmp->next;
+	}
+
+	if (tmp == NULL)
+		return (n);
+
+	prev->next = tmp->next;
+	free(tmp);
+	return (n);
 }
+
