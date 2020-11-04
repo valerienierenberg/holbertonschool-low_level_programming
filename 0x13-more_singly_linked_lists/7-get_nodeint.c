c9a363fad4de3cb71aebd2294b7a3b9ab5e3b474
valerienierenberg <valerie.nierenberg@gmail.com>
2020-11-04 14:28:51 +0000

    Assign *traverse to value of head

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,10 +13,8 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 
-listint_t *traverse;
+listint_t *traverse = head;
 unsigned int i;
-
-traverse = head;
 
 while (i != index)
 {
