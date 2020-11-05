8ce012fe0e7d281b4530fefcfa8f53a22d2c37c3
valerienierenberg <valerie.nierenberg@gmail.com>
2020-11-05 15:51:10 +0000

    Add 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,1 +1,23 @@
 #include "holberton.h"
+
+/**
+* set_bit- Function that sets the value of a bit to 1 at a given index
+* @n: pointer to bit o set value to 1
+* @index: index of bit
+* 
+* Return: 1 if it worked, -1 if error occurred
+*/
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+
+unsigned int i;
+
+if (index > 32)
+return (-1);
+
+i = 1 << index;
+*n = *n | i;
+return (1);
+
+}
