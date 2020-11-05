1eae6a27d43a08216c6256ad27a36a446047ed13
valerienierenberg <valerie.nierenberg@gmail.com>
2020-11-05 15:37:26 +0000

    Add 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,1 +1,24 @@
 #include "holberton.h"
+
+/**
+* print_binary- Function that prints the binary representation of a number
+* @n: number to convert to binary
+* 
+* Return: void
+*/
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+
+unsigned int i;
+
+if (index > 32)
+return (-1);
+
+i = 1 << index;
+i = i & n;
+i = i >> index;
+
+return (i);
+
+}
