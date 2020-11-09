5d9975bc381de4ccf3164382807e283c70ed9add
valerienierenberg <valerie.nierenberg@gmail.com>
2020-11-09 14:51:55 +0000

    Add README.md with file descriptions, header file holberton.h with all function prototypes, and empty task files.

---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,13 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+size_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+
+#endif
