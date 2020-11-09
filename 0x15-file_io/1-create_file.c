41e193a4aeb46109f8a1ec6bd44ed20efa346517
valerienierenberg <valerie.nierenberg@gmail.com>
2020-11-09 17:07:15 +0000

    Add 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,1 +1,41 @@
 #include "holberton.h"
+
+/**
+* create_file - Function that craetes a file
+* @filename: name of file to be created
+* @text_content: content of the text file to be created
+* (NULL terminated string to write to the file)
+* Return: 1 on success, -1 on failure
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+int fd;
+int wr;
+
+if (filename == NULL)
+return (-1);
+
+fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
+
+if (fd == -1)
+{
+close(fd);
+return (-1);
+}
+
+if (text_content == NULL)
+{
+close(fd);
+return (1);
+}
+
+wr = write(fd, text_content, strlen(text_content));
+
+close (fd);
+
+if (wr == -1)
+return (-1);
+
+return (1);
+}
