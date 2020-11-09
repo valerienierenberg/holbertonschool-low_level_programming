88d0a9adeb967a68ce9e5a319fd413f675c9f1fe
valerienierenberg <valerie.nierenberg@gmail.com>
2020-11-09 16:45:24 +0000

    Fix spacing

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,31 +18,36 @@ ssize_t rd;
 ssize_t wr;
 
 if (filename == NULL)
-return (0);
+	return (0);
 
 /* READ */
 fd = open(filename, O_RDONLY);
 if (fd == -1)
-return (0);
+	return (0);
 
 buf = malloc(sizeof(char) * letters);
+
 if (buf == NULL)
-return (0);
+	return (0);
+
 rd = read(fd, buf, letters);
+
 if (rd == -1)
 {
-free(buf);
-return (0);
+	free(buf);
+	return (0);
 }
+
 /*WRITE*/
 wr = write(STDOUT_FILENO, buf, rd);
 
 if (wr == -1)
 {
-free(buf);
-return (0);
+	free(buf);
+	return (0);
 }
 free(buf);
 close(fd);
 return (wr);
 }
+
