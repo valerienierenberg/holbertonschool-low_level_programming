f9fbf3b3203f1bb28e256855330a8b9e5037a6ed
valerienierenberg <valerie.nierenberg@gmail.com>
2020-12-13 17:14:15 +0000

    Change to for loop

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,7 +13,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *cpy = *h;
 	dlistint_t *new;
-	unsigned int i = 0;
+	unsigned int i;
 
 	if (h == NULL)
 		return (NULL);
