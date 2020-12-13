a2e707fed996678afd01a2d64416ca8f302bf891
valerienierenberg <valerie.nierenberg@gmail.com>
2020-12-13 16:35:51 +0000

    Add NULL check

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -20,6 +20,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	newnode->next = (*head);
 	newnode->prev = NULL;
 
+	if ((*head) != NULL)
+		(*head)->prev = newnode;
+
 	(*head) = newnode;
 
 	return (newnode);
