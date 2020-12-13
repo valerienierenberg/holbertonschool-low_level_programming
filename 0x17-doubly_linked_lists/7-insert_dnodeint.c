049de668b19367f04d2846daab339fb1b5bc2d0b
valerienierenberg <valerie.nierenberg@gmail.com>
2020-12-13 17:03:47 +0000

    Change order

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -21,14 +21,15 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	new = malloc(sizeof(dlistint_t));
-		if (new == NULL)
-		return (NULL);
+	if (cpy == NULL && i == idx)
+		return (add_dnodeint_end(h, n));
 
 	while (cpy != NULL)
 	{
-		if (cpy == NULL && i == idx)
-			return (add_dnodeint_end(h, n));
+
+	new = malloc(sizeof(dlistint_t));
+		if (new == NULL)
+		return (NULL);
 
 		if ((idx - 1) == i)
 	{
