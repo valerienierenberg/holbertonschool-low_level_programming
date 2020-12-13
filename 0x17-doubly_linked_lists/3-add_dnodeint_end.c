16339b34b072204ae10415896eb4030cc65dfbc9
valerienierenberg <valerie.nierenberg@gmail.com>
2020-12-13 16:38:38 +0000

    Add 3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -31,6 +31,9 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	while (lastnode->next != NULL)
 		lastnode = lastnode->next;
 
+	if ((*head) != NULL)
+		(*head)->prev = newnode;
+
 	lastnode->next = newnode;
 
 return (lastnode);
