54e26d9ab7632f544693a83b61b7fb30108335e4
valerienierenberg <valerie.nierenberg@gmail.com>
2020-12-13 16:30:09 +0000

    Add newnode->prev = NULL

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -17,8 +17,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 
 	newnode->n = n;
-	newnode->prev = NULL
 	newnode->next = (*head);
+	newnode->prev = NULL;
 
 	(*head) = newnode;
 
