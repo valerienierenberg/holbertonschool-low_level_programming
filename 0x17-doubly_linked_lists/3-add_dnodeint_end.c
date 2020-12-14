b54ae8e1caae8282bce22a42d2faaceb7602c34d
valerienierenberg <valerie.nierenberg@gmail.com>
2020-12-14 19:34:08 +0000

    Remove possibly superfluous statement

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -12,11 +12,6 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	dlistint_t *newnode;
 	dlistint_t *lastnode;
 
-	/*
-	* if (head == NULL)
-	* return (NULL);
-	*/
-
 	newnode = (dlistint_t *) malloc(sizeof(dlistint_t));
 
 	if (newnode == NULL)
@@ -34,7 +29,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		return ((*head));
 	}
 
-	lastnode = (*head);
+	/* lastnode = (*head); */
 
 	while (lastnode->next != NULL)
 		lastnode = lastnode->next;
@@ -48,7 +43,9 @@ return (lastnode);
 /*
 * declare pointer to lastnode, set that equal to head (it's a copy of list)
 * declare pointer to newnode, malloc size for it (check if malloc worked)
-* set 'next' pointer of newnode to point to NULL (line 26)
-* copy 'n' that is passed to function into newnode (line 27)
-* if there is only one node in the list,
+* set 'next' pointer of newnode to point to NULL (line 22)
+* copy 'n' that is passed to function into newnode (line 23)
+* if there's no list yet, set 'prev' pointer of newnode to point to NULL,
+* and set head to be newnode. return the newnode (line 25-29)
+* 
 */
