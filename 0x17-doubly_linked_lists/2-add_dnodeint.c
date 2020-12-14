ec09ae6b630dec7e824f069678c45967a9c5d13d
valerienierenberg <valerie.nierenberg@gmail.com>
2020-12-14 19:17:43 +0000

    Add comment to 2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -27,3 +27,12 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	return (newnode);
 }
+/*
+* declare pointer to newnode, malloc size for it
+* (check if malloc worked)
+* set 'n' of newnode to be the 'n' passed in to function
+* set 'next' pointer in newnode to point to head (making newnode 1st in list)
+* set 'prev' pointer in newnode to be NULL
+* if head exists, make 'prev' pointer of head point to newnode
+* reset head to be newnode, return address of newnode
+*/
