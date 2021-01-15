992e2b7beccb18d3d611207c4c90217cd658ee6e
valerienierenberg <valerie.nierenberg@gmail.com>
2021-01-15 15:41:28 +0000

    Add NULL check for key

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,6 +11,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	hash_node_t *node;
 
+	if (key == NULL)
+		return (NULL);
+
 	for (node = ht->array[key_index((unsigned char *)key, ht->size) % ht->size];
 	node != 0; node = node->next)
 	{
