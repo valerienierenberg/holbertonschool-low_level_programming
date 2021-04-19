08c9e2a20c44a07316acc187185753d1af1b9a8e
valerienierenberg <valerie.nierenberg@gmail.com>
2021-04-19 16:04:57 +0000

    Modify comments

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -3,8 +3,8 @@
 /**
 * linear_search-  searches for a value in an array of integers using
 * the Linear search algorithm
-* @array: integer array
-* @size: pointer to the first element of the integer array to search
+* @array: pointer to the first element of the array to search in
+* @size: number of elements in array
 * @value: value to search for
 * Return: the first index where value is located
 * If value is not present in array or if array is NULL, function returns -1
--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -26,8 +26,8 @@ void pretty_print(int start, int end, int *array)
 /**
 * binary_search- searches for a value in a sorted array of integers using
 * the Binary search algorithm
-* @array: integer array
-* @size: pointer to the first element of the integer array to search
+* @array: pointer to the first element of the array to search in
+* @size: number of elements in array
 * @value: value to search for
 * Return: the index where value is located
 * If value is not present in array or if array is NULL, function returns -1
