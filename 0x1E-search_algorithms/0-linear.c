ee9ca5adc863fb40ac69a475bedb2edae357eb4c
valerienierenberg <valerie.nierenberg@gmail.com>
2021-04-20 17:31:48 +0000

    unsigned int

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,16 +12,16 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-	unsigned i;
+	unsigned int i;
 
-	if (array != NULL)
+	if (array == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
 	{
-		for (i = 0; i < size; i++)
-		{
-			printf("Value checked array[%d] = [%d]\n", i, array[i]);
-			if (array[i] == value)
-				return (i);
-		}
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
 	}
 	return (-1);
 }
