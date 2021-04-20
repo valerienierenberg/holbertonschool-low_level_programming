488ca0fb0bf8eb88b6e32ec0c5e6d5396beb5a59
valerienierenberg <valerie.nierenberg@gmail.com>
2021-04-20 20:26:54 +0000

    Add 106

---
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -0,0 +1,14 @@
+#include "search_algos.h"
+/**
+* linear_skip- searches for a value in a sorted array of integers using
+* the Binary search algorithm
+* @list: pointer to the first element of the linked list to search in
+* @value: value to search for
+* Return: the FIRST index where value is located
+* Using recursion
+* If value is not present in array or if array is NULL, function returns -1
+*/
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+;
+}
