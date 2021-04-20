428a2ab1e2ae4e592ca2e822e92595fd777e61fa
valerienierenberg <valerie.nierenberg@gmail.com>
2021-04-20 20:28:20 +0000

    Add 103

---
+++ 0x1E-search_algorithms/103-exponential.c
@@ -0,0 +1,17 @@
+#include "search_algos.h"
+#include <math.h>
+
+/**
+* exponential_search- searches for a value in a sorted array of ints using
+* the Jump search algorithm
+* (using the square root of the size of the array as the jump step.)
+* @array: pointer to the first element of the array to search in
+* @size: number of elements in array
+* @value: value to search for
+* Return: the first index where value is located
+* If value is not present in array or if array is NULL, function returns -1
+*/
+int exponential_search(int *array, size_t size, int value)
+{
+;
+}
