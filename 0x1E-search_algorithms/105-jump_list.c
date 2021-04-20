ec47d18508178f6f3cbd1137e2ca074c72d22392
valerienierenberg <valerie.nierenberg@gmail.com>
2021-04-20 20:25:47 +0000

    add 105

---
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -0,0 +1,15 @@
+#include "search_algos.h"
+/**
+* jump_list- searches for a value in a sorted array of integers using
+* the Binary search algorithm
+* @list: pointer to the first element of the linked list to search in
+* @size: number of elements in array
+* @value: value to search for
+* Return: the FIRST index where value is located
+* Using recursion
+* If value is not present in array or if array is NULL, function returns -1
+*/
+listint_t *jump_list(listint_t *list, size_t size, int value)
+{
+;
+}
