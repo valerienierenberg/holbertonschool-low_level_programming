f2218c41e3f71799b27ddfd4ca85ff854c9b27a6
valerienierenberg <valerie.nierenberg@gmail.com>
2021-04-20 17:34:32 +0000

    betty

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -18,10 +18,10 @@
  */
 typedef struct skiplist_s
 {
-    int n;
-    size_t index;
-    struct skiplist_s *next;
-    struct skiplist_s *express;
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
 } skiplist_t;
 
 /**
@@ -36,9 +36,9 @@ typedef struct skiplist_s
  */
 typedef struct listint_s
 {
-    int n;
-    size_t index;
-    struct listint_s *next;
+	int n;
+	size_t index;
+	struct listint_s *next;
 } listint_t;
 
 int linear_search(int *array, size_t size, int value);
