8cb4cb8e35810ed676872cb99f918df24e159b06
valerienierenberg <valerie.nierenberg@gmail.com>
2021-04-20 17:30:36 +0000

    Change to array !=NULL

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,14 +14,14 @@ int linear_search(int *array, size_t size, int value)
 {
 	unsigned i;
 
-	if (array == NULL)
-		return (-1);
-
-	for (i = 0; i < size; i++)
+	if (array != NULL)
 	{
-		printf("Value checked array[%d] = [%d]\n", i, array[i]);
-		if (array[i] == value)
-			return (i);
+		for (i = 0; i < size; i++)
+		{
+			printf("Value checked array[%d] = [%d]\n", i, array[i]);
+			if (array[i] == value)
+				return (i);
+		}
 	}
 	return (-1);
 }
