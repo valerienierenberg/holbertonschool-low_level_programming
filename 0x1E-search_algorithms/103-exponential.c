3a8236c17828a0f18a796b09b6a3f2f496c1678f
valerienierenberg <valerie.nierenberg@gmail.com>
2021-04-22 16:04:18 +0000

    Add binary search

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -13,5 +13,16 @@
 */
 int exponential_search(int *array, size_t size, int value)
 {
-;
+	unsigned int i;
+
+	if (array == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
 }
